55c692686f2466cfe69d2b00c41a538bbbcf82ab
tsrkzy <tsrmix@gmail.com>
2020-10-26 17:39:34 +0900

    board drag

--- src/components/organisms/SvgBoard.vue
+++ src/components/organisms/SvgBoard.vue
@@ -8,45 +8,13 @@
       </label>
       <div v-if="debug">
         <div>
-          <h5>BOARD</h5>
-          <div>
-            <label>
-              <span>Zoom:{{ Zoom }}%</span>
-              <input type="range" min="20" max="300" step="10" v-model="Zoom" />
-            </label>
-          </div>
-          <div>
-            <label>
-              <span>XIntercept:{{ XIntercept }}px</span>
-              <input
-                type="range"
-                min="0"
-                max="300"
-                step="10"
-                v-model="XIntercept"
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              <span>YIntercept:{{ YIntercept }}px</span>
-              <input
-                type="range"
-                min="0"
-                max="300"
-                step="10"
-                v-model="YIntercept"
-              />
-            </label>
-          </div>
-        </div>
-        <div>
+          <ha-button @click="onResetBoard(1)">RESET BOARD x1</ha-button>
+          <ha-button @click="onResetBoard(0.5)">RESET BOARD x0.5</ha-button>
+          <ha-button @click="onResetBoard(2)">RESET BOARD x2</ha-button>
           <h5>MAPS</h5>
           <div :key="m.id" v-for="m in maps">
             {{ m.id }}, {{ m.offsetX }}, {{ m.offsetY }}
           </div>
-        </div>
-        <div>
           <h5>PAWNS</h5>
           <div :key="p.id" v-for="p in pawns">
             {{ p.id }}, {{ p.offsetX }}, {{ p.offsetY }}
@@ -61,75 +29,73 @@
         width: svgWidth,
         height: svgHeight
       }"
+      @mousedown="onMouseDown($event)"
     >
       <g
         :id="`board_${activeBoard.id}`"
         v-if="activeBoard"
-        :style="{
-          transform: `scale(${Z}) translate(${XIntercept}px, ${YIntercept}px)`
-        }"
-        @mousedown="onMouseDown(activeBoard.id, $event)"
+        :style="{ transform: `${transform}` }"
       >
-        <rect
-          width="3000"
-          height="3000"
-          x="0"
-          y="0"
-          style="fill: rosybrown;"
-        ></rect>
         <svg-map v-for="m in maps" :key="m.id" :map-id="m.id"></svg-map>
         <svg-pawn v-for="p in pawns" :key="p.id" :pawn-id="p.id"></svg-pawn>
         <circle r="2" cx="0" cy="0" style="fill: red;stroke: none;"></circle>
-        <text>O: {{ XIntercept }},{{ YIntercept }} - {{ activeBoard.id }}</text>
+        <text>O: {{ transform }} - {{ activeBoard.id }}</text>
       </g>
     </svg>
   </div>
 </template>
 
 <script>
+import HaButton from "@/components/atoms/HaButton";
 import SvgMap from "@/components/organisms/SvgMap";
 import SvgPawn from "@/components/organisms/SvgPawn";
+
 export default {
   name: "SvgBoard",
-  components: { SvgPawn, SvgMap },
+  components: { HaButton, SvgPawn, SvgMap },
   methods: {
+    onResetBoard(z) {
+      this.transform = `matrix(${z}, 0, 0, ${z}, 0, 0)`;
+    },
     resize(width, height) {
       this.svgWidth = width;
       this.svgHeight = height;
     },
-    onMouseDown(boardId, e) {
-      const idString = `board_${this.activeBoard.id}`;
-      const $el = document.getElementById(idString);
+    onMouseDown(e) {
+      const $el = document.getElementById("svg-table");
       $el.classList.add("drag");
 
       const downX = e.clientX;
       const downY = e.clientY;
 
-      const ix = parseFloat(this.XIntercept);
-      const iy = parseFloat(this.YIntercept);
+      /* boardの座標系へ変換する行列 */
+      const $ = document.getElementById(`board_${this.activeBoard.id}`);
+      const $$ = $.getCTM();
+
+      function globalToLocal(dx, dy, ctm) {
+        /* 変位をglobalからDOMローカルの座標系へ変換 */
+        return new DOMMatrix([1, 0, 0, 1, dx, dy]).multiply(ctm);
+      }
 
       const onMove = e => {
         e.stopPropagation();
-        const dx = e.clientX - downX;
-        const dy = e.clientY - downY;
+        const t = globalToLocal(e.clientX - downX, e.clientY - downY, $$);
 
-        this.XIntercept = ix + dx / this.Z;
-        this.YIntercept = iy + dy / this.Z;
+        this.transform = `${t}`;
       };
 
       const onMouseUp = e => {
         e.stopPropagation();
         console.log("SvgBoard.onMouseUp"); // @DELETEME
-        /* 新しいpawnの座標 === 画面マウス位置 - オフセット */
-        const dx = e.clientX - downX;
-        const dy = e.clientY - downY;
+        const t = globalToLocal(e.clientX - downX, e.clientY - downY, $$);
 
-        this.XIntercept = ix + dx / this.Z;
-        this.YIntercept = iy + dy / this.Z;
+        this.transform = `${t}`;
+        $el.classList.remove("drag");
         $el.removeEventListener("mousemove", onMove);
         $el.removeEventListener("mouseup", onMouseUp);
         $el.removeEventListener("mouseleave", onMouseUp);
       };
+
       $el.addEventListener("mousemove", onMove, false);
       $el.addEventListener("mouseup", onMouseUp, false);
       $el.addEventListener("mouseleave", onMouseUp, false);
@@ -164,20 +130,18 @@ export default {
       }
       const { width, height } = $el.getBoundingClientRect();
       return { width, height };
-    },
-    Z() {
-      return this.Zoom / 100;
     }
   },
   data() {
+    const zoom = [50, 100, 200][Math.floor(Math.random() * 3)];
+    const z = zoom / 100;
+    const m = new DOMMatrix([z, 0, 0, z, 0, 0]).inverse();
+    const defaultTransform = `${m}`;
     return {
       debug: false,
       svgWidth: 0,
       svgHeight: 0,
-      XIntercept: 0,
-      YIntercept: 0,
-      Zoom: [50, 100, 200][Math.floor(Math.random() * 3)]
-      // Zoom: 100
+      transform: defaultTransform
     };
   }
 };
