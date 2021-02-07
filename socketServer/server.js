a7f037e475aef05efee302f6e659922d9fa4e934
tsrkzy <tsrmix@gmail.com>
2021-02-07 23:06:17 +0900

    node container

--- socketServer/server.js
+++ socketServer/server.js
@@ -11,7 +11,7 @@ const express = require("express");
 
 const app = express();
 app.get("/", (req, res) => {
-  res.send("hello node-express!");
+  res.send("hello hehehe!");
 });
 
 const PORT = 3000;
