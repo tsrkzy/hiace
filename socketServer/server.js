05990e4a2f25f6ad1ca1c6508f1cab86420491ca
tsrkzy <tsrmix@gmail.com>
2021-02-07 21:36:49 +0900

    node container

---
+++ socketServer/server.js
@@ -0,0 +1,21 @@
+/*-----------------------------------------------------------------------------
+ - Copyright (c) 2021.                                                        -
+ - @tsrkzy/Github.                                                            -
+ - tsrmix@gmail.com                                                           -
+ - All rights reserved.                                                       -
+ -----------------------------------------------------------------------------*/
+
+"use strict";
+
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello node-express!");
+});
+
+const PORT = 3000;
+const HOST = "0.0.0.0";
+app.listen(PORT, HOST);
+
+console.log(`express running at http:${HOST}:${PORT}`);
