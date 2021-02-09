1585b77166227ef5ab951266b48941f5dd5a8ad6
tsrkzy <tsrmix@gmail.com>
2021-02-09 18:00:13 +0900

    wss://

--- socketServer/server.js
+++ socketServer/server.js
@@ -7,28 +7,44 @@
 
 "use strict";
 
+const https = require("https");
+const fs = require("fs");
 const WS = require("ws");
 const { SocketClient, onMessage } = require("./SocketClient");
 
-const wss = new WS.Server({
-  port: 3000,
-  perMessageDeflate: {
-    zlibDeflateOptions: {
-      chunkSize: 1024,
-      memLevel: 7,
-      level: 3
-    },
-    zlibInflateOptions: {
-      chunkSize: 10 * 1024
-    },
-    clientNoContextTakeover: true,
-    serverNoContextTakeover: true,
-    serverMaxWindowBits: 10,
+/* wss:// */
+const ssl_server_crt = "/var/ssl/fullchain.pem";
+const ssl_server_key = "/var/ssl/privkey.pem";
 
-    concurrencyLimit: 10,
-    threshold: 1024
-  }
+const options = {
+  key: fs.readFileSync(ssl_server_key),
+  cert: fs.readFileSync(ssl_server_crt)
+};
+const secure_server = https.createServer(options);
+const wss = new WS.Server({
+  server: secure_server
 });
+
+/* ws://*/
+// const wss = new WS.Server({
+//   port: 3000,
+//   perMessageDeflate: {
+//     zlibDeflateOptions: {
+//       chunkSize: 1024,
+//       memLevel: 7,
+//       level: 3
+//     },
+//     zlibInflateOptions: {
+//       chunkSize: 10 * 1024
+//     },
+//     clientNoContextTakeover: true,
+//     serverNoContextTakeover: true,
+//     serverMaxWindowBits: 10,
+//
+//     concurrencyLimit: 10,
+//     threshold: 1024
+//   }
+// });
 
 wss.on("connection", ws => {
   console.log("connection");
@@ -42,4 +58,5 @@ wss.on("connection", ws => {
   });
 });
 
+secure_server.listen(3000);
 console.log("start socket-server");
