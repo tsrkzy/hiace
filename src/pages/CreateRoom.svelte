d169e3fc1996a9372bac8242b274c5d9c22e9fab
tsrkzy <tsrmix@gmail.com>
2023-09-08 07:16:46 +0900

    部屋の作成

--- src/pages/CreateRoom.svelte
+++ src/pages/CreateRoom.svelte
@@ -3,24 +3,33 @@
   import { authenticateWithPopUp } from "../util/googleAuthProvider";
   import { UserCollectionService } from "../service/collection/UserCollectionService";
 
+  const GAME_SYSTEM_LIST = [{ system: "Cthulhu", name: "クトゥルフ神話TRPG" },
+    { system: "Cthulhu7th", name: "新クトゥルフ神話TRPG" },
+    { system: "Kamigakari", name: "神我狩" },
+    { system: "SwordWorld", name: "ソードワールドRPG" },
+    { system: "SwordWorld2.0", name: "ソードワールド2.0" },
+    { system: "SwordWorld2.5", name: "ソードワールド2.5" },]
+
   const { authorized, setAuth } = useAuth();
   const { fetchUserByEmail, createUser } = UserCollectionService()
 
-  export const handleClick = () => {
+  $: roomName = "";
+  $: gameSystem = "null";
+  $: disableCreateRoom = !roomName || gameSystem === "null" ;
+
+  export const handleLogIn = () => {
     return authenticateWithPopUp()
       .then(async (a) => {
         /* Googleログイン後 */
         setAuth(a)
 
         /* ユーザ情報があれば取得し、なければ作る */
-        let user = await fetchUserByEmail(a.Email)
+        const user = await fetchUserByEmail(a.Email)
         if (!user) {
           console.log("no user found by Email.");
-          user = await createUser(a);
-          console.log("user created.", user);
+          const u = await createUser(a);
+          console.log("user created.", u);
         }
-
-        /* 部屋の作成 */
       })
       .catch(e => {
         console.error(e);
@@ -28,6 +37,32 @@
 
   }
 
+  export const handleClick = () => {
+    console.log("CreateRoom.handleClick");
+    if (!authorized) {
+      throw new Error("not authorized");
+    }
+
+    /* 部屋の作成 */
+    console.log("roomName");
+    console.log(roomName);
+    console.log("gameSystem");
+    console.log(gameSystem);
+
+    /* @TODO */
+
+  }
+
+  export const handleRoomNameInput = (e: InputEvent) => {
+    console.log("CreateRoom.handleRoomNameInput", e);
+    const { target: { value = "" } } = e;
+    roomName = value.trim();
+  }
+
+  export const handleGameSystemChange = (e: InputEvent) => {
+    console.log("CreateRoom.handleGameSystemChange", e);
+    gameSystem = e.target.value;
+  }
 </script>
 
 <main>
@@ -35,25 +70,34 @@
     "$authorized: "{$authorized}
     <fieldset class="create-room__fieldset">
       <legend>部屋の作成</legend>
-      <div>
-        <label>
-          <span>部屋名</span>
-          <input
-              type="text"
-              value=""
-              placeholder="必須"
-          >
-        </label>
-      </div>
-      <div>
-        <button
-            disabled="{$authorized}"
-            on:click={handleClick}
-        >作成
-        </button>
-      </div>
       {#if !$authorized}
-        <p>Googleアカウントでのログインが必要です</p>
+        <div>
+          <p>Googleアカウントでのログインが必要です</p>
+          <button on:click={handleLogIn}>loggin</button>
+        </div>
+      {:else }
+        <div>
+          <label>
+            <span>部屋名</span>
+            <input
+                type="text"
+                value=""
+                placeholder="必須"
+                on:input={handleRoomNameInput}
+            >
+          </label>
+          <select on:input={handleGameSystemChange}>
+            <option value="null" selected disabled>ダイスシステム(必須)</option>
+            {#each GAME_SYSTEM_LIST as { system, name }}}
+              <option value="{system}">{name}</option>
+            {/each}
+          </select>
+          <button
+              disabled="{disableCreateRoom}"
+              on:click={handleClick}
+          >作成
+          </button>
+        </div>
       {/if}
     </fieldset>
   </div>
