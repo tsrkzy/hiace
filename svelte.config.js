7e4c296a4defd861db43157e0a72383fbeb7f489
tsrkzy <tsrmix@gmail.com>
2023-11-01 00:27:46 +0900

    config: disable svelte a11y wawrning

--- svelte.config.js
+++ svelte.config.js
@@ -2,4 +2,10 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   preprocess: vitePreprocess(),
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith("a11y-")) {
+      return;
+    }
+    handler(warning);
+  },
 };
