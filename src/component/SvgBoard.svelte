6383b35abbf35264b52ed8d258c1c54caff7fa2e
tsrkzy <tsrmix@gmail.com>
2023-11-05 11:57:13 +0900

    feature: move board

--- src/component/SvgBoard.svelte
+++ src/component/SvgBoard.svelte
@@ -10,15 +10,63 @@
   import { useBoards } from "../model/store/boards";
   import SvgMapChip from "./SvgMapChip.svelte";
   import { useMapChips } from "../model/store/mapChips";
+  import { toCSS } from "../util/style";
 
   const { activeBoard } = useBoards()
   const { mapChips } = useMapChips()
 
-  let boardCSSTransformStyle = ""
+
+  const zoom = 100;
+  const z = zoom / 100;
+  let transformMatrix = `${new DOMMatrix([z, 0, 0, z, 0, 0]).inverse()}`;
+  $: boardStyleString = toCSS({ transform: `${transformMatrix}` });
 
   const onMouseDown = (e: MouseEvent) => {
-    console.log("SvgBoard.onMouseDown", e);
+    console.log("SvgBoard.onMouseDown");
+    e.stopPropagation();
+    e.preventDefault();
+
+
+    const elSvg = document.getElementById("svg-table") as HTMLElement;
+    elSvg.classList.add("drag");
+
+    const downX = e.clientX;
+    const downY = e.clientY;
+
+    /* boardの座標系をglobalへ変換する行列 */
+    const boardEl = document.getElementById(`board_${$activeBoard?.id}`) as HTMLElement&SVGGElement;
+    const ctmBoard = boardEl.getCTM() as DOMMatrix
+
+    const onMove = (e: MouseEvent) => {
+      console.log("SvgBoard.onMove");
+      e.stopPropagation();
+      const t = globalToLocal(e.clientX - downX, e.clientY - downY, ctmBoard);
+      transformMatrix = `${t}`
+    }
+
+    const onMouseUp = (e: MouseEvent) => {
+      console.log("SvgBoard.onMouseUp");
+      e.stopPropagation();
+      const t = globalToLocal(e.clientX - downX, e.clientY - downY, ctmBoard);
+      transformMatrix = `${t}`
+      elSvg.classList.remove("drag");
+
+      elSvg.removeEventListener("mousemove", onMove);
+      elSvg.removeEventListener("mouseup", onMouseUp);
+      elSvg.removeEventListener("mouseleave", onMouseUp);
+    }
+
+    elSvg.addEventListener("mousemove", onMove, false);
+    elSvg.addEventListener("mouseup", onMouseUp, false);
+    elSvg.addEventListener("mouseleave", onMouseUp, false);
+
   }
+
+  function globalToLocal(dx: number, dy: number, ctm = new DOMMatrix()) {
+    /* 変位をglobalからDOMローカルの座標系へ変換 */
+    return new DOMMatrix([1, 0, 0, 1, dx, dy]).multiply(ctm);
+  }
+
   const onWheel = (e: MouseEvent) => {
     console.log("SvgBoard.onWheel", e);
   }
@@ -31,7 +79,7 @@
     on:mousedown={(e)=>onMouseDown(e)}
     on:wheel={(e)=>onWheel(e)}
 >
-  <g id={`board_${$activeBoard?.id}`} style={boardCSSTransformStyle}>
+  <g id={`board_${$activeBoard?.id}`} style={boardStyleString}>
     <text class="crosshair-text" x="0" y="-3">O</text>
     <!--      <path-->
     <!--          d={crossHair}-->
