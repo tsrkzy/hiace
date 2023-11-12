1c156748110ef3fbc95f7c8c21ce405a09781189
tsrkzy <tsrmix@gmail.com>
2023-11-12 10:35:08 +0900

    refactor: map-edit

--- src/component/float/MapEdit.svelte
+++ src/component/float/MapEdit.svelte
@@ -10,19 +10,23 @@
   import { useMapChips } from "../../model/store/mapChips";
   import { Float } from "../../model/Float";
   import { updateMapChip } from "../../model/service/MapChipService";
+  import ImageTileSelector from "../image/ImageTileSelector.svelte";
+  import { useImageSources } from "../../model/store/imageSources";
 
   export let float: Float = {} as Float;
 
 
   const { mapChips } = useMapChips();
+  const { imageSources } = useImageSources()
 
   $: floatId = float?.id;
 
   $: mapChipId = float?.args?.mapChipId;
 
-  $: map = (() => {
+  $: mapChip = (() => {
     return $mapChips.find(m => m.id === mapChipId);
   })()
+
 
   const onChangeDragLockHandler = async (e: Event) => {
     console.log("MapEdit.onChangeDragLockHandler");
@@ -45,17 +49,39 @@
 
     await updateMapChip({ mapChipId, criteria: { scalePp }, })
   }
+
+  const onChangeImageSource = async (e: Event) => {
+    if (!mapChipId) {
+      return;
+    }
+    console.log("MapEdit.onChangeImageSource");
+    e.stopPropagation();
+    const currentTarget = e.currentTarget as HTMLInputElement;
+    const image = currentTarget.value
+    await updateMapChip({ mapChipId, criteria: { image }, })
+  }
 </script>
 
 
 <p>floatid: {floatId}</p>
 <p>mapId: {mapChipId}</p>
 <label>
-  <input type="checkbox" checked={map?.dragLock} on:change={(e)=>onChangeDragLockHandler(e)}/>
+  <input type="checkbox" checked={mapChip?.dragLock} on:change={(e)=>onChangeDragLockHandler(e)}/>
   <span>位置を固定する</span>
 </label>
 
 <fieldset>
-  <legend>サイズの拡縮: {map?.scalePp}%</legend>
-  <input type="range" min="20" max="300" step="5" value="{map?.scalePp}" on:change={(e)=>onChangeScaleHandler(e)}/>
+  <legend>サイズの拡縮: {mapChip?.scalePp}%</legend>
+  <input type="range" min="20" max="300" step="5" value="{mapChip?.scalePp}" on:change={(e)=>onChangeScaleHandler(e)}/>
 </fieldset>
+{mapChip?.image}
+<div style="display: flex;flex-wrap: wrap;flex-direction: row;">
+  {#each $imageSources as imgSrc (imgSrc.id)}
+    <ImageTileSelector
+        name={`map-chip-editor_image-select_float-${floatId}`}
+        checkedId={mapChip?.image}
+        imageSource={imgSrc}
+        onChange={onChangeImageSource}
+    ></ImageTileSelector>
+  {/each}
+</div>
