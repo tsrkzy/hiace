b52f1c50912bdd13c4929387fa4de0ff9ec2c061
tsrkzy <tsrmix@gmail.com>
2023-11-16 22:58:04 +0900

    feature: chat manager

--- src/component/float/ChatManager.svelte
+++ src/component/float/ChatManager.svelte
@@ -9,9 +9,40 @@
   import ChatLogViewer from "../chat/ChatLogViewer.svelte";
   import Checkbox from "../input/Checkbox.svelte";
   import Button from "../button/Button.svelte";
+  import { useAuth } from "../../model/store/auth";
+  import { useCharacters } from "../../model/store/characters";
+  import { useUsers } from "../../model/store/users";
+  import { useAliases } from "../../model/store/aliases";
+  import { useChannels } from "../../model/store/channels";
 
   export let float = {} as Float;
 
+  const CHARACTER_ID_NULL = "NULL";
+  const ALIAS_ID_NULL = "NULL";
+  let characterId = CHARACTER_ID_NULL;
+  let aliasId = ALIAS_ID_NULL;
+
+  const { email } = useAuth()
+  const { myUserId } = useUsers();
+  const { characters } = useCharacters();
+  const { aliases } = useAliases();
+  const { channels } = useChannels();
+
+  $: myCharacters = $characters.filter((c) => c.owner === $myUserId);
+  $: character = $characters.find((c) => c.id === characterId);
+  $: myCharacterAliases = $aliases.filter((a) => a.character === characterId);
+
+
+  const onChangeCharacter = (e: Event) => {
+    console.log("ChatManager.onChangeCharacter", e);
+    const { value } = e.currentTarget as HTMLSelectElement;
+    characterId = value;
+    const character = $characters.find((c) => c.id === characterId);
+    aliasId = character?.activeAlias || ALIAS_ID_NULL;
+  }
+  const onChangeAlias = (e: Event) => {
+    console.log("ChatManager.onChangeAlias", e);
+  }
   const onChangeShowAlias = (e: Event) => {
     console.log("ChatManager.onChangeShowAlias", e);
   };
@@ -21,14 +52,25 @@
 <ChatLogViewer floatId={float.id}></ChatLogViewer>
 <fieldset>
   <legend>チャット設定</legend>
+  {characterId} {aliasId}
   <div style="white-space: nowrap">
+    <select on:change={(e)=>onChangeCharacter(e)}>
+      <option value={CHARACTER_ID_NULL}>{$email}(PL)</option>
+      {#each myCharacters as character (character.id)}
+        <option value={character.id}>{character.name}</option>
+      {/each}
+    </select>
+    <select on:change={(e)=>onChangeAlias(e)}>
+      <option value={ALIAS_ID_NULL} disabled selected={characterId==="NULL"}>立絵</option>
+      {#each myCharacterAliases as alias (alias.id)}
+        <option value={alias.id} selected={alias.id===character?.activeAlias}>{alias.name}</option>
+      {/each}
+    </select>
     <select>
-      <option>キャラクタ</option>
-    </select><select>
-    <option>立絵</option>
-  </select>
-    <select>
-      <option>チャンネル</option>
+      <option value="NULL" selected>チャンネル</option>
+      {#each $channels as channel (channel.id)}
+        <option value={channel.id}>{channel.name}</option>
+      {/each}
     </select>
     <select>
       <option>ダイス</option>
